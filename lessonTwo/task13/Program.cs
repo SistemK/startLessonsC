643e51a6f216fd228e5ffa1c4d8117090e9c2f08
Anikin-K <anikin-k@gaz-is.ru>
2022-12-19 13:37:56 +0300

    Lesson 2 task 13

---
+++ lessonTwo/task13/Program.cs
@@ -0,0 +1,31 @@
+﻿int getNumber (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))        
+        {
+            string strResult = $"{result}";
+            if (strResult.Length != 3)
+            {
+                Console.WriteLine("Необходимо ввести трехзначное число");
+                Console.WriteLine("-----------------------------------");
+            }
+            else 
+            {
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введите корректные данные.");
+            Console.WriteLine("-------------------------.");
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Поиск второго символа в числе");
+int number = getNumber("Введите трехзначное число");
+Console.WriteLine(number%10);
