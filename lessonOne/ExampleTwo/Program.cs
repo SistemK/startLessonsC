0d4c21e73f44c9abb95bfb868c82d7ce3891857e
Anikin-K <anikin-k@gaz-is.ru>
2022-12-19 10:39:29 +0300

    Example Two

---
+++ lessonOne/ExampleTwo/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Максимальное из трех чисел");
+Console.WriteLine("Введите чисто A: ");
+int numberA = int.Parse(Console.ReadLine()?? "Введено некоректное значение, необходимо ввести цифру");
+Console.WriteLine("Введите чисто B: ");
+int numberB = int.Parse(Console.ReadLine()?? "Введено некоректное значение, необходимо ввести цифру");
+Console.WriteLine("Введите чисто С: ");
+int numberC = int.Parse(Console.ReadLine()?? "Введено некоректное значение, необходимо ввести цифру");
+if (numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine($"Максиамльное из трех - A: {numberA}");
+}
+else if (numberB > numberC)
+{
+    Console.WriteLine($"Максиамльное из трех - B: {numberB}");
+}
+else
+{
+    Console.WriteLine($"Максиамльное из трех - C: {numberC}");
+}
