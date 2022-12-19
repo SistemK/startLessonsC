325d272171165ccfe05d891516619ccae42c8429
Anikin-K <anikin-k@gaz-is.ru>
2022-12-19 10:54:14 +0300

    Example Four

---
+++ lessonOne/ExampleFour/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Вывод всех четных чисел от 1 до N");
+Console.WriteLine("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine() ?? "Введено некоректное значение, необходимо ввести цифру");
+int counter = 1;
+while (counter <= numberN)
+{
+    if (counter % 2 == 0)
+    {
+        Console.WriteLine($"{counter}");
+    }
+    counter ++;
+}
