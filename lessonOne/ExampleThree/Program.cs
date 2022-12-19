6781d3905601e6d7a1d2ddbee3b6ccb68fbcf1d8
Anikin-K <anikin-k@gaz-is.ru>
2022-12-19 10:44:44 +0300

    Example Three

---
+++ lessonOne/ExampleThree/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Проверка четное ли число");
+Console.WriteLine("Введите чисто: ");
+int numberA = int.Parse(Console.ReadLine()?? "Введено некоректное значение, необходимо ввести цифру");
+if ( numberA % 2 == 0)
+{
+    Console.WriteLine($"Число четное: {numberA}");
+}
+else 
+{
+    Console.WriteLine($"Число не четное: {numberA}");
+}
