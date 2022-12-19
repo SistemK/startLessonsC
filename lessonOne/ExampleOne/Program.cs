971f9f2d527b3caf838a5d5e41e8126fd7d2ecf1
Anikin-K <anikin-k@gaz-is.ru>
2022-12-19 10:30:50 +0300

    Example One

---
+++ lessonOne/ExampleOne/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("пределение большего числа из двух");
+Console.WriteLine("Введите чисто A: ");
+int numberA = int.Parse(Console.ReadLine()?? "Введено некоректное значение, необходимо ввести цифру");
+Console.WriteLine("Введите чисто B: ");
+int numberB = int.Parse(Console.ReadLine()?? "Введено некоректное значение, необходимо ввести цифру");
+if (numberA > numberB)
+{
+    Console.WriteLine($"Число A больше числа B: {numberA}");
+}
+else
+{
+    Console.WriteLine($"Число B больше числа A: {numberB}");
+}
+
