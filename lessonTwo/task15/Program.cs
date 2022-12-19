0211fd1f6576b166ac13e12711c4fb7841129ae2
Anikin-K <anikin-k@gaz-is.ru>
2022-12-19 13:47:58 +0300

    Lesson 2 task 15

---
+++ lessonTwo/task15/Program.cs
@@ -0,0 +1,30 @@
+﻿int checkWeekDays (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0 && result < 8)
+        {
+            if (result > 5)
+            {
+                Console.WriteLine("Выходной день");
+                break;
+            }
+            else 
+            {
+                Console.WriteLine("Будний день");
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Необходимо выбрать цифру от 1 до 7"); 
+            Console.WriteLine("----------------------------------"); 
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Проверка является ли цифры дня недели выходным.");
+int number = checkWeekDays("Введите день недели");
