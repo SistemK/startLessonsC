3861ed6dcb292d1da88e6f1ef7b10a97d29cc3e0
Anikin-K <anikin-k@gaz-is.ru>
2022-12-24 22:54:18 +0300

    add task 19, lesson 3

---
+++ lessonThree/task19/Program.cs
@@ -0,0 +1,44 @@
+﻿void checkPalindrome (string message)
+{
+    int check = 0;
+    Console.WriteLine(message);
+    while(true)
+    {
+        if (int.TryParse(Console.ReadLine(), out check))
+        {
+            string strCheck = $"{check}";
+            int count = strCheck.Length - 1;
+            int result = 0;
+            for (int i = 0; i < count / 2; i++)
+            {   
+                if (strCheck[i] == strCheck[count -i])
+                {
+                    result = 1;
+                }
+                else
+                {
+                    result = 0;
+                    break;
+                }
+            }
+            if (result == 1)
+            {
+                Console.WriteLine("Число палиндром");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Число не палиндром");
+                break;
+            }
+
+        }
+        else 
+        {
+            Console.WriteLine("Необходимо ввести цифру!");
+            Console.WriteLine("------------------------");
+        }
+    }
+}
+
+checkPalindrome("Введите число");
