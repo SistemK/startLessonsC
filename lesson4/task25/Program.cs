8fab494534a39dd4194d04c5d0f595be0ad11e27
Anikin-K <anikin-k@gaz-is.ru>
2023-01-14 11:32:59 +0300

    Lesson 4 task 25

---
+++ lesson4/task25/Program.cs
@@ -0,0 +1,33 @@
+﻿int getNumber (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректные данные."); 
+        }
+    }
+    return result;
+}
+
+int degree (int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+Console.WriteLine("Возведение числа A в натуральную степень B");
+int numberA = getNumber("Введите число A: ");
+int numberB = getNumber("Введите число B: ");
+int degreeAB = degree(numberA, numberB);
+Console.WriteLine("Число " + numberA + " в степени " + numberB + " равняется " + degreeAB);
