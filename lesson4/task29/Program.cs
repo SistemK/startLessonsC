c337d7df674bcb00c0d151d855184aaef7e484e8
Anikin-K <anikin-k@gaz-is.ru>
2023-01-15 22:46:07 +0300

    Lesson 4 task 29

---
+++ lesson4/task29/Program.cs
@@ -0,0 +1,22 @@
+﻿
+void getArr (string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        string text = Console.ReadLine();
+        string[] textArr  = text.Split(',');   
+        if (textArr.Length == 8)
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", textArr));
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Необходимо ввести массив из 8 символов, а у вас из " + textArr.Length);
+            Console.WriteLine("----------------------------------------------------------");
+        }
+    }
+}
+
+getArr("Введите элеметны массива через запятую");
