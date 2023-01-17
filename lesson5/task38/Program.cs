3561a7c9d5cb17d263e8fd6b703824d216e5858a
Anikin-K <anikin-k@gaz-is.ru>
2023-01-17 22:54:14 +0300

    Lesson 5 task 38

---
+++ lesson5/task38/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+int getNumber (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректные данные."); 
+        }
+    }
+    return result;
+}
+
+int[] initArray (int number)
+{
+    int[] result = new int[number];
+    Random rnd = new Random();
+    for (int i = 0; i < number; i++)
+    {
+        result[i] = rnd.Next(0,100);
+    }
+    return result;
+}
+
+void PrintArray(int [] array)
+{
+foreach (int i in array)
+{
+    Console.Write($"{i} ");
+}
+    Console.WriteLine();
+}
+
+void DiffMinMax (int [] array)
+{
+    int minElement = array[0];
+    int maxElement = array[0];
+    foreach (int i in array)
+    {
+            if (i > maxElement)
+                maxElement = i;
+            if (i < minElement)
+                minElement = i;
+    }
+    Console.WriteLine($"Разница между минимальным и максимальным элементом массива равняется {maxElement - minElement}");
+}
+
+int number = getNumber("Введите размер массива: ");
+int[] arr = initArray(number);
+PrintArray(arr);
+DiffMinMax(arr);
