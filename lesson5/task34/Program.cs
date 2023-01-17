8f87ea8d5dcb274aa13eb40d807b0bf91169403f
Anikin-K <anikin-k@gaz-is.ru>
2023-01-17 21:41:58 +0300

    Lesson 5 task 34

---
+++ lesson5/task34/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int getNumber (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректные данные."); 
+        }
+    }
+    return result;
+}
+
+int[] initArray (int number)
+{
+    int[] result = new int[number];
+    Random rnd = new Random();
+    for (int i = 0; i < number; i++)
+    {
+        result[i] = rnd.Next(100, 999);
+    }
+    return result;
+}
+
+void PrintArray(int [] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+    Console.WriteLine();
+}
+
+void sumEvenNumbers(int [] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum++;
+        }
+    }
+    Console.WriteLine($"Количесво четных чисел в массиве равно {sum}");
+}
+
+int number = getNumber("Введите размер массива: ");
+int[] arr = initArray(number);
+PrintArray(arr);
+sumEvenNumbers(arr);
