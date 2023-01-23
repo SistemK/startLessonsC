7a3bad08f58a9ed3086c95437e9ea4939713d562
Anikin-K <anikin-k@gaz-is.ru>
2023-01-23 17:02:06 +0300

    leson 6 task 41 && 43

---
+++ lesson6/task41/Program.cs
@@ -0,0 +1,23 @@
+﻿
+void getArr (string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        int sum = 0;
+        string text = Console.ReadLine() ?? "NULL";
+        int[] textArr  = text.Split(',').Select(Int32.Parse).ToArray();;   
+        for (int i = 0; i < textArr.Length; i++)
+        {
+            if (textArr[i] > 0)
+            {
+                sum++;
+            }
+        }
+        Console.WriteLine($"Вы ввели {sum} чисел больше нуля");
+        break;
+    }
+}
+
+Console.WriteLine ("Вычисление количества введеных чисел больше нуля");
+getArr("Введите числа через запятую: ");
---
+++ lesson6/task43/Program.cs
@@ -0,0 +1,40 @@
+﻿
+/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+int getNumber (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректные данные."); 
+        }
+    }
+    return result;
+}
+
+void findCross(float b1, float k1, float b2, float k2)
+{
+    float x = 0;
+    float y = 0;
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения заданных прямых - ({x}, {y}).");
+}
+
+Console.WriteLine("Нахождение точки пересечения двух прямых");
+float b1 = getNumber("Задайте координату b1 первой прямой ");
+float k1 = getNumber("Задайте координату k1 первой прямой ");
+float b2 = getNumber("Задайте координату b2 первой прямой ");
+float k2 = getNumber("Задайте координату k2 первой прямой ");
+findCross(b1, k1, b2, k2);
